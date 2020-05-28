af43562eb7aca370de86274c681f882e6edd8a55
Vimhash <perlazajohao@gmail.com>
2020-05-28 18:41:08 -0500

    cambios

--- App.js
+++ App.js
@@ -1,80 +1,112 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Alert, Button, TextInput } from "react-native";
-import { Permissions } from "expo";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import { db } from "./firebase";
 import email from "react-native-email";
+import Constants from "expo-constants";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 export default class App extends Component {
-  state = {
-    title: "Mi ubicacion",
-    hideMap: true,
-    hideMarkers: true,
-    longitud: null,
-    latitud: null,
-    locations: [],
-    userName: "",
-    secuencia: "",
-  };
-
-  componentDidMount() {
-    // this._enableGPS();
-    db.ref("locations").on("value", (element) => {
-      let locations = [];
-      let id = [];
-      element.forEach((item) => {
-        locations.push(item.val());
-      });
-      locations.map((element) => id.push(element.id));
-      id.reverse();
-      this.setState({ secuencia: id[0] + 1 });
-    });
-    db.ref("locations").on("value", (element) => {
-      let locations = [];
-      element.forEach((item) => {
-        locations.push(item.val());
-      });
-
-      this.setState({ locations });
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      hideMarkers: true,
+      hideApp: true,
+      hideError: true,
+      hideCoords: true,
+      loading: "Cargando ...",
+      latitud: "",
+      longitud: "",
+      getLocations: [],
+      userName: "",
+      secuencia: "",
+      location: null,
+      errorMessage: null,
+    };
   }
 
-  _enableGPS = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+  componentDidMount() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        errorMessage: "Oops, GPS no funciona en un emulador de Android",
+        hideError: false,
+      });
+    } else {
+      this._getLocationAsync();
+      db.ref("locations").on("value", (element) => {
+        let locations = [];
+        let id = [];
+        element.forEach((item) => {
+          locations.push(item.val());
+        });
+        locations.map((element) => id.push(element.id));
+        id.reverse();
+        this.setState({ secuencia: id[0] + 1 });
+      });
+      db.ref("locations").on("value", (element) => {
+        let getLocations = [];
+        element.forEach((item) => {
+          getLocations.push(item.val());
+        });
 
-    if (status !== "granted") {
-      console.log("Sin permiso");
+        this.setState({ getLocations });
+      });
     }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permiso de localización denegado",
+        hideError: false,
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location, hideApp: false, loading: "" });
   };
 
   handleText = (text) => {
     this.setState({ userName: text });
   };
 
-  geo = () => {
-    navigator.geolocation.getCurrentPosition(
-      (posicion) => {
-        const longitud = posicion.coords.longitude;
-        const latitud = posicion.coords.latitude;
-
-        this.setState({
-          longitud,
-          latitud,
-          hideMap: false,
-          title: "Refrescar Ubicación",
-        });
-      },
-      (error) => Alert(error.message),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-    );
+  realTimeLocation = () => {
+    setInterval(() => {
+      this.geo();
+    }, 1000);
   };
 
-  shareLocation = () => {
-    if (this.state.userName === "") {
-      alert("Ingrese un nombre de usuario");
+  geo = () => {
+    this._getLocationAsync();
+    const longitud = this.state.location.coords.longitude;
+    const latitud = this.state.location.coords.latitude;
+
+    this.setState({
+      longitud,
+      latitud,
+      hideCoords: false,
+    });
+  };
+
+  saveData = () => {
+    if (
+      (this.state.userName === "",
+      this.state.latitud === "",
+      this.state.longitud === "")
+    ) {
+      alert("Ingrese su nombre antes de continuar");
     } else {
-      db.ref("/locations")
-        .push({
+      db.ref("locations/" + this.state.userName + "_location")
+        .set({
           id: this.state.secuencia,
           message: this.state.userName,
           currentLocation: {
@@ -82,14 +114,27 @@ export default class App extends Component {
             longitude: this.state.longitud,
           },
         })
-        .then(
-          this.setState({ hideMarkers: false }),
-          alert("Ubicación compartida con éxito")
-        )
+        .then(this.setState({ hideMarkers: false }, this.updateData()))
         .catch((error) => {
           console.log(error);
         });
     }
+  };
+
+  updateData = () => {
+    setInterval(() => {
+      db.ref("locations/" + this.state.userName + "_location")
+        .update({
+          currentLocation: {
+            latitude: this.state.latitud,
+            longitude: this.state.longitud,
+          },
+        })
+        .then(this.setState({ hideMarkers: false }))
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 5000);
   };
 
   handleEmail = () => {
@@ -114,68 +159,102 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={estilos.contenedor}>
-        <Button title={this.state.title} onPress={this.geo} />
-        {this.state.hideMap ? null : (
-          <View style={estilos.map}>
-            <View style={estilos.header}>
-              <Text style={estilos.text1}>Latitud: {this.state.latitud}</Text>
-              <Text style={estilos.text1}>Longitud: {this.state.longitud}</Text>
-              <Button
-                title="Enviar ubicación al Correo"
-                onPress={this.handleEmail}
-              />
+      <View style={estilos.screen}>
+        {this.state.hideApp ? (
+          <Text
+            style={{
+              color: "green",
+              fontSize: 50,
+            }}
+          >
+            {this.state.loading}
+          </Text>
+        ) : (
+          <View
+            style={{
+              height: "100%",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <View style={{ marginTop: 50, flex: 1 }}>
+              {this.state.hideCoords ? (
+                <Button
+                  title="Empezar"
+                  onPress={() => this.realTimeLocation()}
+                />
+              ) : null}
+              {this.state.hideError ? null : (
+                <Text
+                  style={{
+                    color: "blue",
+                    fontSize: 50,
+                  }}
+                >
+                  {this.state.errorMessage}
+                </Text>
+              )}
+              {this.state.hideCoords ? null : (
+                <View style={{ flexDirection: "row" }}>
+                  <View style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
+                    <Text>Latitud: {this.state.latitud}</Text>
+                    <Text>Longitud: {this.state.longitud}</Text>
+                    <View style={{ paddingVertical: 10 }}>
+                      <Button
+                        title="Enviar Coordenadas"
+                        onPress={() => this.handleEmail()}
+                      />
+                    </View>
+                  </View>
+
+                  {this.state.hideMarkers ? (
+                    <View
+                      style={{ paddingVertical: 10, paddingHorizontal: 10 }}
+                    >
+                      <TextInput
+                        placeholder="username"
+                        underlineColorAndroid="transparent"
+                        style={{
+                          borderColor: "gray",
+                          borderWidth: 1,
+                        }}
+                        onChangeText={this.handleText}
+                      />
+                      <View style={{ paddingVertical: 10 }}>
+                        <Button
+                          title="Compartir Ubicación"
+                          onPress={() => this.saveData()}
+                        />
+                      </View>
+                    </View>
+                  ) : null}
+                </View>
+              )}
             </View>
-            <View
-              style={{
-                marginHorizontal: 25,
-                flexDirection: "row",
-              }}
-            >
-              <TextInput
-                placeholder="username"
-                underlineColorAndroid="transparent"
-                style={{
-                  height: 40,
-                  width: "50%",
-                  borderColor: "gray",
-                  borderWidth: 1,
-                  padding: 5,
-                }}
-                keyboardType={"default"}
-                onChangeText={this.handleText}
-              />
-              <Button
-                title="Compartir Ubicación"
-                onPress={this.shareLocation}
-              />
-            </View>
-            <MapView
-              style={estilos.map1}
-              region={{
-                latitude: this.state.latitud,
-                longitude: this.state.longitud,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-              }}
-            >
-              {/* <Marker
-                coordinate={{
+
+            <View style={estilos.mapcontainer}>
+              <MapView
+                style={estilos.map1}
+                initialRegion={{
                   latitude: this.state.latitud,
                   longitude: this.state.longitud,
+                  latitudeDelta: 0.0922,
+                  longitudeDelta: 0.0421,
                 }}
-                title="Mi Ubicación Actual"
-              /> */}
-              {this.state.hideMarkers
-                ? null
-                : this.state.locations.map((item) => (
-                    <Marker
-                      key={item.id}
-                      coordinate={item.currentLocation}
-                      title={item.message}
-                    />
-                  ))}
-            </MapView>
+              >
+                {this.state.hideMarkers
+                  ? null
+                  : this.state.getLocations.map((item) => (
+                      <Marker
+                        key={item.id}
+                        coordinate={item.currentLocation}
+                        title={item.message}
+                      />
+                    ))}
+              </MapView>
+            </View>
           </View>
         )}
       </View>
@@ -184,41 +263,19 @@ export default class App extends Component {
 }
 
 const estilos = StyleSheet.create({
-  contenedor: {
-    marginTop: 125,
+  screen: {
     height: "100%",
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
-    position: "absolute",
+    flexDirection: "column",
   },
-  header: {
+  mapcontainer: {
+    flex: 3,
     width: "100%",
-    padding: 25,
-  },
-  text: {
-    fontSize: 20,
-    textAlign: "center",
-    padding: 10,
-    margin: 10,
-    borderRadius: 10,
-    backgroundColor: "blue",
-    color: "white",
-  },
-  text1: {
-    fontSize: 15,
-    textAlign: "center",
-    margin: 10,
-    color: "red",
-  },
-  map: {
-    height: "100%",
-    width: "100%",
-    position: "relative",
   },
   map1: {
-    flex: 1,
-    height: 400,
+    height: "100%",
     width: "100%",
   },
 });
